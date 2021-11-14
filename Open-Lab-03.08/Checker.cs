e6561f934395c0f1f13251dc32eb9a581e9af8e8
AdamHaluska <adam.haluska11@gmail.com>
2021-11-14 21:08:45 +0100

    hotovo!

--- Open-Lab-03.08/Checker.cs
+++ Open-Lab-03.08/Checker.cs
@@ -4,9 +4,12 @@ namespace Open_Lab_03._08
 {
     public class Checker
     {
-        public bool IsPlural(string word)
+         if (word.EndsWith("s"))
+                return true;
+            
         {
-            throw new NotImplementedException();
+            else
+                return false;
         }
     }
 }
